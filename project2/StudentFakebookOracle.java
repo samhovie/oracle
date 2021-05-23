dcb5d5e0a7471e4c86e13bd0c468a678637a6469
Samuel Hovie <samhovie@umich.edu>
2021-05-23 18:15:37 -0400

    test

--- project2/StudentFakebookOracle.java
+++ project2/StudentFakebookOracle.java
@@ -374,7 +374,8 @@ public final class StudentFakebookOracle extends FakebookOracle {
                 "WHERE T.tag_subject_id = U.user_id " +
                 "AND T.tag_photo_id = P_INFO.photo_id " +
                 "ORDER BY users DESC, P_INFO.photo_id, U.user_id"
-            );
+            
+                );
 
             int photoCount = 0;
             while (rst.next() && photoCount < num) {
