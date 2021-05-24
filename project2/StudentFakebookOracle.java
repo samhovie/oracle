44df9f78334272fc483af569fd3bb209c0282aa0
Samuel Hovie <samhovie@umich.edu>
2021-05-24 19:57:00 -0400

    update

--- project2/StudentFakebookOracle.java
+++ project2/StudentFakebookOracle.java
@@ -317,55 +317,27 @@ public final class StudentFakebookOracle extends FakebookOracle {
         try (Statement stmt = oracle.createStatement(FakebookOracleConstants.AllScroll, FakebookOracleConstants.ReadOnly)) {
 
 
-        //     stmt.executeUpdate("CREATE OR REPLACE VIEW TOP_PAIRS AS " +
-        //                     //select two ids 
-        //                     "SELECT DISTINCT LEAST(FIRST, SECOND) AS FIRST, GREATEST(FIRST, SECOND) AS SECOND"+
-        //                     " FROM " +  
-        //                     "(SELECT U1.USER_ID AS FIRST, U2.USER_ID AS SECOND, COUNT(T1.TAG_PHOTO_ID) "+ // count number of 
-        //                     "FROM " + UsersTable + 
-        //                     " U1, " + UsersTable + " U2, " + TagsTable + "  T1, " + TagsTable + "  T2 " +
-        //                     //where they're both tag subject of same photo id
-        //                     "WHERE U1.USER_ID = T1.TAG_SUBJECT_ID AND U2.USER_ID = T2.TAG_SUBJECT_ID " + 
-        //                     " AND T1.TAG_PHOTO_ID = T2.TAG_PHOTO_ID AND"+
-        //                     //same gender, not same person
-        //                     " U1.GENDER = U2.GENDER AND " +
-        //                     "U1.USER_ID <> U2.USER_ID AND " +
-        //                     //age difference
-        //                     "((U1.YEAR_OF_BIRTH - U2.YEAR_OF_BIRTH >= 0 AND U1.YEAR_OF_BIRTH - U2.YEAR_OF_BIRTH <= " + yearDiff + 
-        //                     ") OR (U2.YEAR_OF_BIRTH - U1.YEAR_OF_BIRTH >= 0 AND U2.YEAR_OF_BIRTH - U1.YEAR_OF_BIRTH <= " + yearDiff + 
-        //                     ")) "+
-        //                     //and they're not friends 
-        //                     "AND NOT EXISTS(SELECT * FROM " + FriendsTable + " F " +
-        //                     "WHERE (F.USER1_ID = U1.USER_ID AND F.USER2_ID = U2.USER_ID) " + 
-        //                     "OR (F.USER1_ID = U2.USER_ID AND F.USER2_ID = U1.USER_ID)) " +
-
-        //                     "GROUP BY U1.USER_ID, U2.USER_ID " +
-        //                     "ORDER BY COUNT(T1.TAG_PHOTO_ID) DESC, U1.USER_ID ASC, U2.USER_ID ASC) " + 
-        //                     "WHERE ROWNUM <= " + num);
-        
-        // ResultSet rst = stmt.executeQuery("SELECT T.FIRST, T.SECOND, U1.FIRST_NAME, U1.LAST_NAME, U1.YEAR_OF_BIRTH, U2.FIRST_NAME, " +
-        //                                   "U2.LAST_NAME, U2.YEAR_OF_BIRTH, P.PHOTO_ID, P.ALBUM_ID, P.PHOTO_LINK, A.ALBUM_NAME " +
-        //                                   "FROM TOP_PAIRS T, " + UsersTable + " U1, " + UsersTable + " U2, " + TagsTable + 
-        //                                   " T1, " + TagsTable + " T2, " + PhotosTable + " P, " + AlbumsTable + " A " +
-        //                                   "WHERE T.FIRST = U1.USER_ID AND T.SECOND = U2.USER_ID AND T.FIRST = T1.TAG_SUBJECT_ID " + 
-        //                                   "AND T.SECOND = T2.TAG_SUBJECT_ID AND T1.TAG_PHOTO_ID = T2.TAG_PHOTO_ID AND " +
-        //                                   "T1.TAG_PHOTO_ID = P.PHOTO_ID AND P.ALBUM_ID = A.ALBUM_ID " +
-        //                                   "ORDER BY T.FIRST ASC, T.SECOND ASC"
-        //                                   );
-
-
-
-
-
 ResultSet rst = stmt.executeQuery(
-    "SELECT T.FIRST, T.SECOND, U1.FIRST_NAME, U1.LAST_NAME, U1.YEAR_OF_BIRTH, U2.FIRST_NAME, U2.LAST_NAME, U2.YEAR_OF_BIRTH, P.PHOTO_ID, P.ALBUM_ID, P.PHOTO_LINK, A.ALBUM_NAME FROM ( SELECT DISTINCT LEAST(FIRST, SECOND) AS FIRST, GREATEST(FIRST, SECOND) AS SECOND"+
-        " FROM (SELECT U1.USER_ID AS FIRST, U2.USER_ID AS SECOND, COUNT(T1.TAG_PHOTO_ID) FROM " + UsersTable + " U1, " + UsersTable + " U2, " + TagsTable + "  T1, " + TagsTable + "  T2 " +
-            "WHERE U1.USER_ID = T1.TAG_SUBJECT_ID AND U2.USER_ID = T2.TAG_SUBJECT_ID AND T1.TAG_PHOTO_ID = T2.TAG_PHOTO_ID AND U1.GENDER = U2.GENDER AND U1.USER_ID <> U2.USER_ID AND " +
-            "((U1.YEAR_OF_BIRTH - U2.YEAR_OF_BIRTH >= 0 AND U1.YEAR_OF_BIRTH - U2.YEAR_OF_BIRTH <= " + yearDiff + ") OR (U2.YEAR_OF_BIRTH - U1.YEAR_OF_BIRTH >= 0 AND U2.YEAR_OF_BIRTH - U1.YEAR_OF_BIRTH <= " + yearDiff + 
-            ")) AND NOT EXISTS(SELECT * FROM " + FriendsTable + " F WHERE (F.USER1_ID = U1.USER_ID AND F.USER2_ID = U2.USER_ID) OR (F.USER1_ID = U2.USER_ID AND F.USER2_ID = U1.USER_ID)) " +
-            "GROUP BY U1.USER_ID, U2.USER_ID ORDER BY COUNT(T1.TAG_PHOTO_ID) DESC, U1.USER_ID ASC, U2.USER_ID ASC) WHERE ROWNUM <= " + num +
-                      ") T, " + UsersTable + " U1, " + UsersTable + " U2, " + TagsTable + 
-                      " T1, " + TagsTable + " T2, " + PhotosTable + " P, " + AlbumsTable + " A " +
+    "SELECT "+
+    "T.FIRST, T.SECOND, "+
+    "U1.FIRST_NAME, U1.LAST_NAME, U1.YEAR_OF_BIRTH, "+
+    "U2.FIRST_NAME, U2.LAST_NAME, U2.YEAR_OF_BIRTH, "+
+    "P.PHOTO_ID, P.ALBUM_ID, P.PHOTO_LINK, A.ALBUM_NAME "+
+    
+    "FROM (SELECT DISTINCT LEAST(FIRST, SECOND) AS FIRST, GREATEST(FIRST, SECOND) AS SECOND FROM (SELECT U1.USER_ID AS FIRST, U2.USER_ID AS SECOND, COUNT(T1.TAG_PHOTO_ID) "+
+    "FROM " + UsersTable + " U1, " + UsersTable + " U2, " + TagsTable + "  T1, " + TagsTable + "  T2 "+
+        "WHERE "+
+        "U1.USER_ID = T1.TAG_SUBJECT_ID AND U2.USER_ID = T2.TAG_SUBJECT_ID AND T1.TAG_PHOTO_ID = T2.TAG_PHOTO_ID AND "+
+        "U1.GENDER = U2.GENDER AND U1.USER_ID <> U2.USER_ID AND "+
+        
+        "((U1.YEAR_OF_BIRTH - U2.YEAR_OF_BIRTH >= 0 AND U1.YEAR_OF_BIRTH - U2.YEAR_OF_BIRTH <= " + yearDiff + ") OR (U2.YEAR_OF_BIRTH - U1.YEAR_OF_BIRTH >= 0 AND U2.YEAR_OF_BIRTH - U1.YEAR_OF_BIRTH <= " + yearDiff + ")) "+
+            
+            "AND NOT EXISTS "+
+            "(SELECT * FROM " + FriendsTable + " F WHERE "+
+            
+            "(F.USER1_ID = U1.USER_ID AND F.USER2_ID = U2.USER_ID) OR (F.USER1_ID = U2.USER_ID AND F.USER2_ID = U1.USER_ID)) " +
+
+            "GROUP BY U1.USER_ID, U2.USER_ID ORDER BY COUNT(T1.TAG_PHOTO_ID) DESC, U1.USER_ID ASC, U2.USER_ID ASC) WHERE ROWNUM <= " + num + ") T, " + UsersTable + " U1, " + UsersTable + " U2, " + TagsTable + " T1, " + TagsTable + " T2, " + PhotosTable + " P, " + AlbumsTable + " A " +
                       "WHERE T.FIRST = U1.USER_ID AND T.SECOND = U2.USER_ID AND T.FIRST = T1.TAG_SUBJECT_ID AND T.SECOND = T2.TAG_SUBJECT_ID AND T1.TAG_PHOTO_ID = T2.TAG_PHOTO_ID AND T1.TAG_PHOTO_ID = P.PHOTO_ID AND P.ALBUM_ID = A.ALBUM_ID ORDER BY T.FIRST ASC, T.SECOND ASC"
                       );
 
@@ -373,10 +345,8 @@ ResultSet rst = stmt.executeQuery(
         Long user1ID = null;
         Long user2ID = null;
         MatchPair pair = null;
-        while (rst.next()) {
-            //if id is null or both ids equal don't equal the current pos of rst
+        while (rst.next()) {\
             if (user1ID == null || (!user1ID.equals(rst.getLong(1)) && !user2ID.equals(rst.getLong(2)))) {
-                //if pair is not null, 
                 if (pair != null) {
                     results.add(pair);
                 }
@@ -414,8 +384,6 @@ ResultSet rst = stmt.executeQuery(
                         rst.getString(11), rst.getString(12)));
             }
         }
-
-        stmt.executeUpdate("DROP VIEW TOP_PAIRS");
         rst.close();
         stmt.close();
 
