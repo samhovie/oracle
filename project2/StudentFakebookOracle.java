db4766e72303b5edc02cb65a244400e9224de1ef
Samuel Hovie <samhovie@umich.edu>
2021-05-24 20:17:15 -0400

    update

--- project2/StudentFakebookOracle.java
+++ project2/StudentFakebookOracle.java
@@ -385,8 +385,10 @@ ResultSet rst = stmt.executeQuery(
         // }
 
         rst.next();
-        Long uid1 = rst.getLong(1);
-        Long uid2 = rst.getLong(2);
+        // Long uid1 = rst.getLong(1);
+        // Long uid2 = rst.getLong(2);
+        Long user1ID = null;
+        Long user2ID = null;
         MatchPair pair = new MatchPair(new UserInfo(uid1, rst.getString(3), rst.getString(4)), 
                             rst.getInt(5), new UserInfo(uid2, rst.getString(6), rst.getString(7)), rst.getInt(8));
         pair.addSharedPhoto(new PhotoInfo(rst.getInt(9), rst.getInt(10),
